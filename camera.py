06053e0a21684374d30589ca0d7bacfff086fa41
121yaseen <121yaseen@gmail.com>
2020-08-07 07:27:09 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -17,13 +17,11 @@ class VideoCamera(object):
 
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
-        print("Camera Checkpoint 3")
         _, fr = self.video.read()
         gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
         for (x, y, w, h) in faces:
-            print("Camera Checkpoint 4")
             fc = gray_fr[y:y+h, x:x+w]
 
             roi = cv2.resize(fc, (48, 48))
@@ -31,7 +29,6 @@ class VideoCamera(object):
 
             cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
-            print("Camera Checkpoint 5")
 
         _, jpeg = cv2.imencode('.jpg', fr)
         return jpeg.tobytes()
